ff7cd1d1b5f05fb409e153ea923ecb64202d5a5c
czearing <czearing@outlook.com>
2022-02-17 15:01:41 -0800

    Cleaning up code

--- scripts/jest.config.js
+++ scripts/jest.config.js
@@ -1,7 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default {
   displayName: 'react-storybook',
   preset: 'ts-jest',
   collectCoverage: true,
   setupFilesAfterEnv: ['./config/tests.js'],
+  testMatch: ['<rootDir>/src/**/*.test.ts(x)?'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/lib-commonjs/', '/dist/'],
 };
