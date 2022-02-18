a1c2eb32632dde8d44ce84fccd38d3ee514ae4dc
czearing <czearing@outlook.com>
2022-02-18 13:21:23 -0800

    Resolving merge conflicts

--- packages/react-button/jest.config.js
+++ packages/react-button/jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   displayName: 'react-button',
   preset: 'ts-jest',
-<<<<<<< HEAD
-=======
   testEnvironment: 'jest-environment-jsdom-sixteen',
->>>>>>> 56c06662a9c96952b8b33f081629012c8b3ecb6e
   collectCoverage: true,
   setupFilesAfterEnv: ['./config/tests.js'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/lib-commonjs/', '/dist/'],
--- scripts/jest.config.js
+++ scripts/jest.config.js
@@ -17,25 +17,12 @@ import { fileURLToPath } from 'url';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
-<<<<<<< HEAD
-export const JestPreset = {
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-    chalk: 'chalk/source/index.js',
-    '#ansi-styles': 'chalk/source/vendor/ansi-styles/index.js',
-    '#supports-color': 'chalk/source/vendor/supports-color/index.js',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'mts', 'mjs', 'js', 'cjs'],
-=======
 export default {
   displayName: 'react-storybook',
   preset: 'ts-jest',
   collectCoverage: true,
   testEnvironment: 'jest-environment-jsdom-sixteen',
   setupFilesAfterEnv: ['./config/tests.js'],
->>>>>>> 56c06662a9c96952b8b33f081629012c8b3ecb6e
   testMatch: ['<rootDir>/src/**/*.test.ts(x)?'],
   transform: {
     '^.+\\.[jt]sx?$': ['babel-jest', { configFile: path.resolve(dirname, 'babel.config.js') }],
