4be159dbf907fa3922cb92a428cf7a2f00e6e667
czearing <czearing@outlook.com>
2022-02-22 13:55:08 -0800

    Minor change

--- packages/react-button/src/components/Button/Button.types.ts
+++ packages/react-button/src/components/Button/Button.types.ts
@@ -10,35 +10,35 @@ export type ButtonSlots = {
 
 type ButtonCommons = {
   /**
-   * The appearance variant to use for the button.
+   * The appearance variant to use for the Button.
    *
    * @default outline
    */
   appearance?: 'outline' | 'primary' | 'subtle' | 'transparent';
 
   /**
-   * The color type for the button.
+   * The color type for the Button.
    *
    * @default inherit
    */
   color?: 'inherit' | 'brand' | 'secondary' | 'success' | 'error' | 'social' | 'warning' | 'info';
 
   /**
-   * Whether the button should be disabled.
+   * Whether the Button should be disabled.
    *
    * @default false
    */
   disabled?: boolean;
 
   /**
-   * The shape of the button.
+   * The shape of the Button.
    *
    * @default rounded
    */
   shape?: 'rounded' | 'circular' | 'circle' | 'square';
 
   /**
-   * The size of the button.
+   * The size of the Button.
    *
    * @default medium
    */
