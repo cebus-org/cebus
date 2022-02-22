8a9eae4b07f3b1096d8694bd72390696a891cbd9
czearing <czearing@outlook.com>
2022-02-22 11:35:14 -0800

    Adding color stories

--- packages/react-button/src/stories/ToggleButton.stories.tsx
+++ packages/react-button/src/stories/ToggleButton.stories.tsx
@@ -35,6 +35,113 @@ export const BasicToggleButtonExample = () => {
   );
 };
 
+export const ColorToggleButtonExample = () => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.row}>
+        <ToggleButton color="inherit" shape="circular">
+          Inherit
+        </ToggleButton>
+        <ToggleButton checked color="inherit" shape="circular">
+          Inherit
+        </ToggleButton>
+        <ToggleButton disabled color="inherit" shape="circular">
+          Inherit
+        </ToggleButton>
+        <ToggleButton checked disabled color="inherit" shape="circular">
+          Inherit
+        </ToggleButton>
+      </div>
+      <div className={styles.row}>
+        <ToggleButton color="brand" shape="circular">
+          Brand
+        </ToggleButton>
+        <ToggleButton checked color="brand" shape="circular">
+          Brand
+        </ToggleButton>
+        <ToggleButton disabled color="brand" shape="circular">
+          Brand
+        </ToggleButton>
+        <ToggleButton checked disabled color="brand" shape="circular">
+          Brand
+        </ToggleButton>
+      </div>
+      <div className={styles.row}>
+        <ToggleButton color="secondary" shape="circular">
+          Secondary
+        </ToggleButton>
+        <ToggleButton checked color="secondary" shape="circular">
+          Secondary
+        </ToggleButton>
+        <ToggleButton disabled color="secondary" shape="circular">
+          Secondary
+        </ToggleButton>
+        <ToggleButton checked disabled color="secondary" shape="circular">
+          Secondary
+        </ToggleButton>
+      </div>
+      <div className={styles.row}>
+        <ToggleButton color="success" shape="circular">
+          Success
+        </ToggleButton>
+        <ToggleButton checked color="success" shape="circular">
+          Success
+        </ToggleButton>
+        <ToggleButton disabled color="success" shape="circular">
+          Success
+        </ToggleButton>
+        <ToggleButton checked disabled color="success" shape="circular">
+          Success
+        </ToggleButton>
+      </div>
+      <div className={styles.row}>
+        <ToggleButton color="error" shape="circular">
+          Error
+        </ToggleButton>
+        <ToggleButton checked color="error" shape="circular">
+          Error
+        </ToggleButton>
+        <ToggleButton disabled color="error" shape="circular">
+          Error
+        </ToggleButton>
+        <ToggleButton checked disabled color="error" shape="circular">
+          Error
+        </ToggleButton>
+      </div>
+      <div className={styles.row}>
+        <ToggleButton color="warning" shape="circular">
+          Warning
+        </ToggleButton>
+        <ToggleButton checked color="warning" shape="circular">
+          Warning
+        </ToggleButton>
+        <ToggleButton disabled color="warning" shape="circular">
+          Warning
+        </ToggleButton>
+        <ToggleButton checked disabled color="warning" shape="circular">
+          Warning
+        </ToggleButton>
+      </div>
+      <div className={styles.row}>
+        <ToggleButton color="info" shape="circular">
+          Info
+        </ToggleButton>
+        <ToggleButton checked color="info" shape="circular">
+          Info
+        </ToggleButton>
+        <ToggleButton disabled color="info" shape="circular">
+          Info
+        </ToggleButton>
+        <ToggleButton checked disabled color="info" shape="circular">
+          Info
+        </ToggleButton>
+      </div>
+    </div>
+  );
+};
+
 export default {
   title: 'Components/ToggleButton',
   component: ToggleButton,
