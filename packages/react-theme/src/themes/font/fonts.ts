7b2ab8f3af64fd82f41a5ea501fd785b0ffcdbf1
czearing <czearing@outlook.com>
2022-03-02 12:37:50 -0800

    Addressing a bug

--- packages/react-theme/src/themes/font/fonts.ts
+++ packages/react-theme/src/themes/font/fonts.ts
@@ -1,5 +1,9 @@
 import type { Fonts } from '../../types';
-import { fontFamilyTokens, fontOpacityTokens, fontSizeTokens, fontLineHeightTokens, fontWeightTokens } from './index';
+import { fontFamilyTokens } from './fontFamilyTokens';
+import { fontOpacityTokens } from './fontOpacityTokens';
+import { fontSizeTokens } from './fontSizeTokens';
+import { fontLineHeightTokens } from './fontLineHeightTokens';
+import { fontWeightTokens } from './fontWeightTokens';
 
 export const fonts: Fonts = {
   ...fontFamilyTokens,
