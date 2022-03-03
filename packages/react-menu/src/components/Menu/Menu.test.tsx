6a8a0b1af5a2d38622732429c220f04793266acd
czearing <czearing@outlook.com>
2022-03-03 13:08:42 -0800

    Adding more tests

--- packages/react-menu/src/components/Menu/Menu.test.tsx
+++ packages/react-menu/src/components/Menu/Menu.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Menu } from './Menu';
 import type { MenuProps } from '@fluentui/react-menu';
 import { isConformant } from '../../common/isConformant';
+import { MenuTrigger } from '../MenuTrigger/index';
+import { MenuList } from '../MenuList/index';
+import { MenuItem } from '../MenuItem/index';
+import { MenuPopover } from '../MenuPopover/index';
 
 describe('Menu', () => {
   isConformant({
@@ -15,5 +19,17 @@ describe('Menu', () => {
       'consistent-callback-args',
       'make-styles-overrides-win',
     ],
+    requiredProps: {
+      children: [
+        <MenuTrigger key="trigger">
+          <button>MenuTrigger</button>
+        </MenuTrigger>,
+        <MenuPopover key="popover">
+          <MenuList>
+            <MenuItem>Item</MenuItem>
+          </MenuList>
+        </MenuPopover>,
+      ],
+    },
   });
 });
---
+++ packages/react-menu/src/components/MenuItem/MenuItem.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MenuItem } from './MenuItem';
+import type { MenuItemProps } from '@fluentui/react-menu';
+import { render, fireEvent } from '@testing-library/react';
+import { isConformant } from '../../common/isConformant';
+
+describe('MenuItem', () => {
+  isConformant({
+    Component: MenuItem as React.FunctionComponent<MenuItemProps>,
+    displayName: 'MenuItem',
+    disabledTests: ['component-handles-classname', 'make-styles-overrides-win'],
+  });
+
+  it('renders correctly', () => {
+    const result = render(<MenuItem>Test Item</MenuItem>);
+    expect(result.container).toMatchSnapshot();
+  });
+
+  it('renders secondaryContent correctly', () => {
+    const result = render(<MenuItem secondaryContent="test">Test Item</MenuItem>);
+    expect(result.container).toMatchSnapshot();
+  });
+
+  it('renders disabled correctly', () => {
+    const result = render(<MenuItem disabled>Test Item</MenuItem>);
+    expect(result.container).toMatchSnapshot();
+  });
+});
