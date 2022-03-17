1e528b0cb22941b25c74b40f19f5bf9d0358c1a8
czearing <czearing@outlook.com>
2022-03-16 17:43:43 -0700

    Updating tests

--- packages/react-layout/src/components/Container/Container.test.tsx
+++ packages/react-layout/src/components/Container/Container.test.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
 import { Container } from './Container';
-import { fireEvent, render, screen } from '@testing-library/react';
 import { isConformant } from '../../common/isConformant';
 
 describe('Container', () => {
